483fdcb081259193af96b8da2e1b5cb692ce37b7
DonMohsen <mohsendorado@gmail.com>
2025-09-28 12:03:14 +0330

    We got icons yeeeey

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,37 +7,34 @@ import useRoutes from '@/hooks/useRoutes'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import clsx from 'clsx'
 import useSidebarToggle from '@/stores/useSidebarToggle'
+import Image from 'next/image'
 
 
 export default function Sidebar() {
   const router = useRouter()
   const routes=useRoutes();
   // Your route configuration
-  const { isOpen, toggle, open, close } = useSidebarToggle();
 
   return (
-    <aside className={`h-full bg-transparent transition-all duration-300   ${
-      !isOpen ? 'w-20' : 'w-64'
-    }`}>
+    <aside className={`h-full bg-transparent transition-all duration-300 w-[80px]`}>
       <div className="p-4 h-full flex flex-col gap-2">
         {/* Logo/Collapse Header */}
-        <div className={`flex items-center ${!isOpen ? 'justify-center' : 'justify-between'} mb-6`}>
-          {isOpen && <h2 className="text-xl font-semibold"></h2>}
-          <button 
-            onClick={toggle}
-            className="p-2 hover:bg-gray-100 rounded-lg"
-          >
-            {!isOpen ? <ChevronRight  /> : <ChevronLeft  />}
-          </button>
+        <div className={`flex items-center justify-center mb-6`}>
+         
+          
+            {/* {!isOpen ? <ChevronRight  /> : <ChevronLeft  />} */}
+            <Image 
+            onClick={()=>router.push('/')}
+            alt='logo' src={'/favicon.svg'} width={500} height={500} className='w-full h-full cursor-pointer' />
         </div>
 
         {/* Navigation Links */}
-        <nav className="flex-1">
+        <nav className="flex-1 p-4">
           <ul className="space-y-5">
             {routes.map((route) => (
               <li
               key={route.label}>
-              <Link className="flex flex-col gap-2 items-center justify-center cursor-pointer "
+              <Link className={`flex flex-col gap-2  justify-center cursor-pointer items-center `}
               href={route.href}
               >
                     <route.icon className={clsx(`h-7 w-7 text-[#64ce69] transition-all duration-200`,route.active? 'stroke-3':'stroke-1')} />
